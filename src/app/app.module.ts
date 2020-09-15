1347d3f5cbfcad3b0e206136b57d48fa7a9a3312
Magks <magks@github.com>
2020-09-14 17:35:16 -0700

    fixed merge conflicts in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
-import { FormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { EditComponent } from './edit/edit.component'
-=======
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddFormComponent } from './add-form.component';
 import { HttpClientModule } from '@angular/common/http';
->>>>>>> apiV2
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    EditComponent
-=======
+    EditComponent,
     AddFormComponent
->>>>>>> apiV2
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    FormsModule
-=======
+    FormsModule,
     ReactiveFormsModule,
     HttpClientModule
->>>>>>> apiV2git 
   ],
   providers: [],
   bootstrap: [AppComponent]
